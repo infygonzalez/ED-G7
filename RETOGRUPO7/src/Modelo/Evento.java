843dd3d2f767f4e943ef5895a2394f547df222fa
in1dm3-a <in1dm3-a@HZ352035.elorrieta.int>
2025-01-23 12:26:45 +0100

    No se habian añadido bien

--- RETOGRUPO7/src/Modelo/Evento.java
+++ RETOGRUPO7/src/Modelo/Evento.java
@@ -11,5 +11,43 @@ public class Evento {
 		this.nombre = nombre;
 		this.tipo = tipo;
 	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Evento [id=" + id + ", nombre=" + nombre + ", tipo=" + tipo + "]";
+	}
+	
+	
 	
 }
