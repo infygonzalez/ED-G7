83d9abe6d7352101d6a1f78b34a072aa59ab4327
in1dm3-a <in1dm3-a@HZ352035.elorrieta.int>
2025-01-23 12:25:43 +0100

    Añadimos constructores, getters y setters y el metodo toString.

--- RETOGRUPO7/src/Modelo/Evento.java
+++ RETOGRUPO7/src/Modelo/Evento.java
@@ -4,4 +4,12 @@ public class Evento {
 	private String id;
 	private String nombre;
 	private String tipo;
+	
+	
+	public Evento(String id, String nombre, String tipo) {
+		this.id = id;
+		this.nombre = nombre;
+		this.tipo = tipo;
+	}
+	
 }
